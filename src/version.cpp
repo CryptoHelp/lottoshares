6fbc8e382890c0c8733108404801ab86ba17a2d6
FreeTrade <thailandstartup@gmail.com>
2014-06-25 17:45:44 +0700

    Name Changes

--- src/version.cpp
+++ src/version.cpp
@@ -33,10 +33,11 @@ const std::string CLIENT_NAME("Satoshi");
 #    include "build.h"
 #endif
 
-// git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+// git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "$Format:%h$"
-#    define GIT_COMMIT_DATE "$Format:%cD$"
+#    define GIT_COMMIT_ID "dab1cbb"
+#    define GIT_COMMIT_DATE "Sat, 25 Jan 2014 08:32:52 +0100"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
