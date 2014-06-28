db280a683dcebe466be801dbe929030260e7b362
FreeTrade <thailandstartup@gmail.com>
2014-06-28 11:33:51 +0700

    lottoshares case fix

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -85,7 +85,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("LottoShares:%1").arg(address);
+    QString ret = QString("lottoshares:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
